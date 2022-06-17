e143a8881daad6efadbe35401b5b8172e132c84f
Martekode <BrianDesmedt9000@hotmail.com>
2022-06-17 16:50:08 +0200

    excersise 7 complete

--- expert.php
+++ expert.php
@@ -99,3 +99,29 @@ function randomHeroName()
 }
 
 echo "Here is the name: " . combineNames();
+
+
+new_exercise(7);
+function copyright(string $year) {
+    return "&copy; $year BeCode";
+}
+//print the copyright
+echo copyright(date('y'));
+
+new_exercise(8);
+function login(string $email, string $password) {
+    if($email == 'john@example.be' || $password == 'pocahontas') {
+        return 'Welcome John';
+        return ' Smith';
+    }
+    return 'No access';
+}
+
+//do not change anything below
+//should great the user with his full name (John Smith)
+echo login('john@example.be', 'pocahontas');
+//no access
+echo login('john@example.be', 'dfgidfgdfg');
+//no access
+echo login('wrong@example.be', 'wrong');
+//you can change things again!
