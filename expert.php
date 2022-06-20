1d161206f78ed5f7d2cc7cbf06f1978a7d5f1f99
Martekode <BrianDesmedt9000@hotmail.com>
2022-06-20 10:02:13 +0200

    excersise 10 partially complete , beef is still in de stack

--- expert.php
+++ expert.php
@@ -142,3 +142,18 @@ echo isLinkValid('https://google.com');
 echo isLinkValid('http://google.com');
 //VALID link
 echo isLinkValid('http://google.com/test.txt');
+
+
+new_exercise(10);
+
+//Filter the array $areTheseFruits to only contain valid fruits
+//do not change the arrays itself
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+for($i=0; $i < count($areTheseFruits); $i++) {
+    if(!in_array($areTheseFruits[$i], $validFruits)) {
+        array_splice($areTheseFruits,$i,1);
+    }
+}
+var_dump($areTheseFruits);//do not change this
